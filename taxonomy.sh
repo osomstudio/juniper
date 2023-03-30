e4a48d2b921ae0a05b1fde21d721e09add7468f3
bartnovak <b.nowak@osomstudio.com>
2023-03-30 11:25:02 +0200

    Bash for taxonomies

--- taxonomy.sh
+++ taxonomy.sh
@@ -10,11 +10,18 @@ if [[ $full_name =~ ['!@#$%^&*()+'] ]]; then
 fi
 
 #CREATING A SLUG
+variable_name=$(echo $full_name | tr '[:upper:]' '[:lower:]')
+variable_name=$(echo $variable_name | tr ' ' '_')
+
 lowercase_full_name=$(echo $full_name | tr '[:upper:]' '[:lower:]')
-slug_name=$(echo $lowercase_full_name | tr ' ' '-')
+
+slug_name=$(echo $lowercase_full_name | awk '{for(i=1;i<=NF;i++){$i=toupper(substr($i,1,1)) substr($i,2)}}1')
+slug_name=${slug_name// /}
+
+rewrite_name=$(echo $lowercase_full_name | tr ' ' '-')
 
 #CHECK IF A Taxonomy ALREADY EXISTS
-[ -f "./web/app/themes/osom-theme/inc/taxonomies/class-${slug_name}.php" ] && echo "A Taxonomy with this name already exists" && exit
+[ -f "./web/app/themes/osom-theme/inc/Taxonomies/${slug_name}.php" ] && echo "A Taxonomy with this name already exists" && exit
 
 read -p "What's the name of the select post type ? " selected_post_type
 #VALIDATION -IT CAN ONLY HAVE BIG AND SMALL LETTERS, SPACES, DASHES, FLOORS
@@ -23,36 +30,33 @@ if [[ $selected_post_type =~ ['!@#$%^&*()+'] ]]; then
     exit
 fi
 
-cp "dev/taxonomy.txt" "./web/app/themes/osom-theme/inc/taxonomies/class-${slug_name}.php"
+cp "dev/taxonomy.txt" "./web/app/themes/osom-theme/inc/Taxonomies/${slug_name}.php"
 
 #GET TO CPT FOLDER
-cd ./web/app/themes/osom-theme/inc/taxonomies/
+cd ./web/app/themes/osom-theme/inc/Taxonomies/
 
 
 searchSlug="replace_taxonomy_slug"
 searchName="replace_taxonomy_name"
-searchClassName="replace_taxonomy_slugTaxonomy"
-replaceClassName="${slug_name}Taxonomy"
+searchRewrite="replace_rewrite_name"
 searchPostSlug="selected_post_type"
 
-sed -i'' -e "s/$searchSlug/$slug_name/" class-${slug_name}.php
-sed -i'' -e "s/$searchName/$full_name/" class-${slug_name}.php
-sed -i'' -e "s/$searchPostSlug/$selected_post_type/g" class-${slug_name}.php
+sed -i'' -e "s/$searchSlug/$slug_name/" ${slug_name}.php
+sed -i'' -e "s/$searchName/$full_name/" ${slug_name}.php
+sed -i'' -e "s/$searchRewrite/$rewrite_name/" ${slug_name}.php
+sed -i'' -e "s/$searchPostSlug/$selected_post_type/g" ${slug_name}.php
 
-rm class-${slug_name}.php-e
+rm ${slug_name}.php-e
 
-phpcbf "class-${slug_name}.php"
+phpcbf "${slug_name}.php"
 
 cd ..
 
 composer dump-autoload -o
 
-echo '$osom_taxonomy_replace_taxonomy_slug = new \Osom\replace_taxonomy_slugTaxonomy();' >> include.php
+echo '$osom_taxonomy_replace_rewrite_name = new \Osom\Taxonomies\replace_taxonomy_slug();' >> include.php
 
 sed -i'' -e "s/$searchSlug/$slug_name/" include.php
-rm include.php-e
-
-sed -i'' -e "s/$searchClassName/$replaceClassName/" include.php
 rm include.php-e
 
 phpcbf "include.php"
