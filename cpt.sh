1013799351f263055bf316fda06103c26221b323
bartnovak <b.nowak@osomstudio.com>
2023-03-30 10:12:42 +0200

    CPT Autoload changes

--- cpt.sh
+++ cpt.sh
@@ -11,35 +11,36 @@ fi
 
 #CREATING A SLUG
 lowercase_full_name=$(echo $full_name | tr '[:upper:]' '[:lower:]')
-slug_name=$(echo $lowercase_full_name | tr ' ' '-')
+slug_name=$(echo $lowercase_full_name | awk '{for(i=1;i<=NF;i++){$i=toupper(substr($i,1,1)) substr($i,2)}}1')
+slug_name=${slug_name// /}
 
 #CHECK IF A CPT ALREADY EXISTS
-[ -f "./web/app/themes/osom-theme/inc/cpt/class-${slug_name}.php" ] && echo "A CPT with this name already exists" && exit
+[ -f "./web/app/themes/osom-theme/inc/Cpt/${slug_name}.php" ] && echo "A CPT with this name already exists" && exit
 
 
-cp "dev/cpt.txt" "./web/app/themes/osom-theme/inc/cpt/class-${slug_name}.php"
+cp "dev/cpt.txt" "./web/app/themes/osom-theme/inc/Cpt/${slug_name}.php"
 
 #GET TO CPT FOLDER
-cd ./web/app/themes/osom-theme/inc/cpt/
+cd ./web/app/themes/osom-theme/inc/Cpt/
 
 
 searchSlug="replace_cpt_slug"
 searchName="replace_cpt_name"
 searchClassName="replace_cpt_slugCPT"
-replaceClassName="${slug_name}CPT"
+replaceClassName="${slug_name}"
 
-sed -i'' -e "s/$searchSlug/$slug_name/" class-${slug_name}.php
-sed -i'' -e "s/$searchName/$full_name/" class-${slug_name}.php
+sed -i'' -e "s/$searchSlug/$slug_name/" ${slug_name}.php
+sed -i'' -e "s/$searchName/$full_name/" ${slug_name}.php
 
-rm class-${slug_name}.php-e
+rm ${slug_name}.php-e
 
-phpcbf "class-${slug_name}.php"
+phpcbf "${slug_name}.php"
 
 cd ..
 
 composer dump-autoload -o
 
-echo '$osom_cpt_replace_cpt_slug = new \Osom\replace_cpt_slugCPT();' >> include.php
+echo '$osom_cpt_replace_cpt_slug = new \Osom\Cpt\replace_cpt_slugCPT();' >> include.php
 
 sed -i'' -e "s/$searchSlug/$slug_name/" include.php
 rm include.php-e
