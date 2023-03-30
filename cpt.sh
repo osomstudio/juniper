77f7055820407e80379a8b5ad55017afbe84acca
bartnovak <b.nowak@osomstudio.com>
2023-03-30 10:31:23 +0200

    CPT creating

--- cpt.sh
+++ cpt.sh
@@ -12,9 +12,13 @@ fi
 #CREATING A SLUG
 variable_name=$(echo $full_name | tr '[:upper:]' '[:lower:]')
 variable_name=$(echo $variable_name | tr ' ' '_')
+
 lowercase_full_name=$(echo $full_name | tr '[:upper:]' '[:lower:]')
+
 slug_name=$(echo $lowercase_full_name | awk '{for(i=1;i<=NF;i++){$i=toupper(substr($i,1,1)) substr($i,2)}}1')
 slug_name=${slug_name// /}
+
+rewrite_name=$(echo $lowercase_full_name | tr ' ' '-')
 
 #CHECK IF A CPT ALREADY EXISTS
 [ -f "./web/app/themes/osom-theme/inc/Cpt/${slug_name}.php" ] && echo "A CPT with this name already exists" && exit
@@ -28,11 +32,13 @@ cd ./web/app/themes/osom-theme/inc/Cpt/
 
 searchSlug="replace_cpt_slug"
 searchName="replace_cpt_name"
+searchRewrite="replace_rewrite_name"
 searchClassName="replace_cpt_slugCPT"
 replaceClassName="${slug_name}"
 
 sed -i'' -e "s/$searchSlug/$slug_name/" ${slug_name}.php
 sed -i'' -e "s/$searchName/$full_name/" ${slug_name}.php
+sed -i'' -e "s/$searchRewrite/$rewrite_name/" ${slug_name}.php
 
 rm ${slug_name}.php-e
 
