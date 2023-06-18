b9259e1f037e5e6d972d05f9b2b4eae65e8b138c
Szymon Polaczy <s.polaczy@osomstudio.com>
2023-06-18 13:32:34 +0200

    Fixed getting the filepath each time + PHCBF

--- web/app/mu-plugins/juniper-commands.php
+++ web/app/mu-plugins/juniper-commands.php
@@ -12,6 +12,12 @@
 if ( defined( 'WP_CLI' ) && WP_CLI && class_exists( 'WP_CLI' ) ) {
 	if ( ! class_exists( 'Juniper_Commands' ) ) {
 		class Juniper_Commands extends WP_CLI_Command {
+			private $mu_plugins;
+
+			public function __construct() {
+				$this->mu_plugins = dirname( __FILE__ );
+			}
+
 			private function get_name( $assoc_args ) {
 				if ( ! key_exists( 'name', $assoc_args ) ) {
 					WP_CLI::error( 'I need to know the name' );
@@ -61,7 +67,7 @@ if ( defined( 'WP_CLI' ) && WP_CLI && class_exists( 'WP_CLI' ) ) {
 
 				extract( $this->get_needed_names( $og_name ) );
 
-				if ( file_exists( dirname(__FILE__) . "/../themes/juniper-theme/inc/Cpt/$slug_name.php" ) ) {
+				if ( file_exists( $this->mu_plugins . "/../themes/juniper-theme/inc/Cpt/$slug_name.php" ) ) {
 					WP_CLI::error( 'Custom post type already exists' );
 				}
 
@@ -71,18 +77,18 @@ if ( defined( 'WP_CLI' ) && WP_CLI && class_exists( 'WP_CLI' ) ) {
 					array( 'replace_rewrite_name', $rewrite_name ),
 				);
 
-				$file_contents = file_get_contents( dirname(__FILE__) . "/../../../dev/cpt.txt" );
+				$file_contents = file_get_contents( $this->mu_plugins . '/../../../dev/cpt.txt' );
 				foreach ( $replace_array as $search_replace ) {
 					$file_contents = str_replace( $search_replace[0], $search_replace[1], $file_contents );
 				}
 
-				file_put_contents(dirname(__FILE__) . "/../themes/juniper-theme/inc/Cpt/$slug_name.php", $file_contents);
+				file_put_contents( $this->mu_plugins . "/../themes/juniper-theme/inc/Cpt/$slug_name.php", $file_contents );
 
-				$new_class   = "\$juniper_$slug_name = new \Juniper\cpt\\$slug_name();" . PHP_EOL;
-				file_put_contents(dirname(__FILE__) . "/../themes/juniper-theme/inc/include.php", $new_class, FILE_APPEND);
+				$new_class = "\$juniper_$slug_name = new \Juniper\cpt\\$slug_name();" . PHP_EOL;
+				file_put_contents( $this->mu_plugins . '/../themes/juniper-theme/inc/include.php', $new_class, FILE_APPEND );
 
-				shell_exec( "phpcbf --standard=WordPress-Extra " . dirname(__FILE__) . "/../themes/juniper-theme/inc/Cpt/$slug_name.php" );
-				shell_exec( "phpcbf --standard=WordPress-Extra " . dirname(__FILE__) . "/../themes/juniper-theme/inc/include.php" );
+				shell_exec( 'phpcbf --standard=WordPress-Extra ' . $this->mu_plugins . "/../themes/juniper-theme/inc/Cpt/$slug_name.php" );
+				shell_exec( 'phpcbf --standard=WordPress-Extra ' . $this->mu_plugins . '/../themes/juniper-theme/inc/include.php' );
 			}
 
 			/**
@@ -92,7 +98,7 @@ if ( defined( 'WP_CLI' ) && WP_CLI && class_exists( 'WP_CLI' ) ) {
 			 *
 			 * [--name]
 			 * : The name of custom taxonomy you want
-			 * 
+			 *
 			 * [--post]
 			 * : The name of the custom post type that this taxonomy should be attached to
 			 *
@@ -116,7 +122,7 @@ if ( defined( 'WP_CLI' ) && WP_CLI && class_exists( 'WP_CLI' ) ) {
 
 				extract( $this->get_needed_names( $og_name ) );
 
-				if ( file_exists( dirname(__FILE__) . "/../themes/juniper-theme/inc/Taxonomies/$slug_name.php" ) ) {
+				if ( file_exists( $this->mu_plugins . "/../themes/juniper-theme/inc/Taxonomies/$slug_name.php" ) ) {
 					WP_CLI::error( 'Taxonomy already exists' );
 				}
 
@@ -127,17 +133,17 @@ if ( defined( 'WP_CLI' ) && WP_CLI && class_exists( 'WP_CLI' ) ) {
 					array( 'selected_post_type', $post_cpt ),
 				);
 
-				$file_contents = file_get_contents( dirname(__FILE__) . "/../../../dev/taxonomy.txt" );
+				$file_contents = file_get_contents( $this->mu_plugins . '/../../../dev/taxonomy.txt' );
 				foreach ( $replace_array as $search_replace ) {
 					$file_contents = str_replace( $search_replace[0], $search_replace[1], $file_contents );
 				}
 
-				file_put_contents(dirname(__FILE__) . "/../themes/juniper-theme/inc/Taxonomies/$slug_name.php", $file_contents);
+				file_put_contents( $this->mu_plugins . "/../themes/juniper-theme/inc/Taxonomies/$slug_name.php", $file_contents );
 
-				$new_class   = "\$juniper_$slug_name = new \Juniper\Taxonomies\\$slug_name();" . PHP_EOL;
-				file_put_contents(dirname(__FILE__) . "/../themes/juniper-theme/inc/include.php", $new_class, FILE_APPEND);
+				$new_class = "\$juniper_$slug_name = new \Juniper\Taxonomies\\$slug_name();" . PHP_EOL;
+				file_put_contents( $this->mu_plugins . '/../themes/juniper-theme/inc/include.php', $new_class, FILE_APPEND );
 
-				shell_exec( 'phpcbf --standard=WordPress-Extra ' . dirname(__FILE__) . '/../themes/juniper-theme/inc/include.php' );
+				shell_exec( 'phpcbf --standard=WordPress-Extra ' . $this->mu_plugins . '/../themes/juniper-theme/inc/include.php' );
 			}
 
 
@@ -169,33 +175,33 @@ if ( defined( 'WP_CLI' ) && WP_CLI && class_exists( 'WP_CLI' ) ) {
 					WP_CLI::error( 'Name can only have: letters, spaces, dashes, floors' );
 				}
 
-				$lowercase_name = strtolower($og_name);
-				$slug_name = str_replace( ' ', '_', $lowercase_name );
+				$lowercase_name = strtolower( $og_name );
+				$slug_name      = str_replace( ' ', '_', $lowercase_name );
 
 				if ( file_exists( "../themes/juniper-theme/blocks/$slug_name/" ) ) {
 					WP_CLI::error( 'Block already exists' );
 				}
 
-				mkdir(dirname(__FILE__) . "/../themes/juniper-theme/blocks/$slug_name/", 0755);
+				mkdir( $this->mu_plugins . "/../themes/juniper-theme/blocks/$slug_name/", 0755 );
 
-				$keywords = "";
-				if (key_exists('keywords', $assoc_args)) {
+				$keywords = '';
+				if ( key_exists( 'keywords', $assoc_args ) ) {
 					$keywords = $assoc_args['keywords'];
 				}
 
-				$description = "";
-				if (key_exists('description', $assoc_args)) {
+				$description = '';
+				if ( key_exists( 'description', $assoc_args ) ) {
 					$description = $assoc_args['description'];
 				}
 
-				file_put_contents(dirname(__FILE__) . "/../themes/juniper-theme/blocks/$slug_name/scripts.js", '');
-				file_put_contents(dirname(__FILE__) . "/../themes/juniper-theme/blocks/$slug_name/ajax.js", '');
+				file_put_contents( $this->mu_plugins . "/../themes/juniper-theme/blocks/$slug_name/scripts.js", '' );
+				file_put_contents( $this->mu_plugins . "/../themes/juniper-theme/blocks/$slug_name/ajax.js", '' );
 
 				$css = ".$slug_name {}\n\n" .
 				"body.wp-admin {\n" .
 				"\t.$slug_name {}\n" .
-				"}";
-				file_put_contents(dirname(__FILE__) . "/../themes/juniper-theme/blocks/$slug_name/style.scss", $css);
+				'}';
+				file_put_contents( $this->mu_plugins . "/../themes/juniper-theme/blocks/$slug_name/style.scss", $css );
 
 				$php = "<?php\n\n" .
 				"add_action('wp_enqueue_scripts', function() {\n" .
@@ -204,14 +210,14 @@ if ( defined( 'WP_CLI' ) && WP_CLI && class_exists( 'WP_CLI' ) ) {
 				"\t\$theme_path = get_template_directory_uri();\n\n" .
 				"\t\twp_enqueue_style('$slug_name-css', \$theme_path . '/dist/blocks/$slug_name/style.css', array(), \$time, 'all');\n" .
 				"\t\twp_enqueue_script('$slug_name-js', \$theme_path . '/dist/blocks/$slug_name/script.js', array(), \$time, true);\n" .
-				"\t}\n" . 
+				"\t}\n" .
 				"});\n\n" .
 				"add_filter(\n" .
 				"\t'timber/acf-gutenberg-blocks-data/$slug_name',\n" .
 				"\tfunction( \$context ) {\n" .
 				"\treturn \$context;\n" .
-				"});";
-				file_put_contents(dirname(__FILE__) . "/../themes/juniper-theme/blocks/$slug_name/functions.php", $php);
+				'});';
+				file_put_contents( $this->mu_plugins . "/../themes/juniper-theme/blocks/$slug_name/functions.php", $php );
 
 				$html = "{#\n" .
 				"\tTitle: $og_name\n" .
@@ -225,10 +231,10 @@ if ( defined( 'WP_CLI' ) && WP_CLI && class_exists( 'WP_CLI' ) ) {
 				"\tSupportsAlign: left right full\n" .
 				"\tSupportsMode: true\n" .
 				"\tSupportsMultiple: true\n" .
-				"#}";
-				file_put_contents(dirname(__FILE__) . "/../themes/juniper-theme/views/blocks/$slug_name.twig", $html);
+				'#}';
+				file_put_contents( $this->mu_plugins . "/../themes/juniper-theme/views/blocks/$slug_name.twig", $html );
 
-				shell_exec( "phpcbf --standard=WordPress-Extra " . dirname(__FILE__) . "/../themes/juniper-theme/Blocks/$slug_name/functions.php" );
+				shell_exec( 'phpcbf --standard=WordPress-Extra ' . $this->mu_plugins . "/../themes/juniper-theme/Blocks/$slug_name/functions.php" );
 			}
 		}
 
