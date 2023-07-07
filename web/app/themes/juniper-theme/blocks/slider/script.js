40bab4949efc0c0fa193b0d7554480febb1b2fef
Will Nahmens <will@wp-stars.com>
2023-07-07 14:06:55 +0200

    update timeline slider

--- web/app/themes/juniper-theme/blocks/slider/script.js
+++ web/app/themes/juniper-theme/blocks/slider/script.js
@@ -3,15 +3,21 @@ let slideNum = 1,
     totalNumSlides = document.querySelectorAll(".timeline-slide").length
     sliderBtn = document.getElementById("sliderScroll")
 sliderBtn.addEventListener("click", () => {
-    console.log('click', totalNumSlides)
     // get the next child in the parent
     if(slideNum === totalNumSlides) {
         slideNum = 1
     } else {
         slideNum++
     }
-    console.log(slideNum)
     let targetSlide = document.querySelector(`.timeline-slider [data-slide-num='${slideNum}']`)
-    console.log(targetSlide)
     targetSlide.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" })
+
+    let allPoints = document.querySelectorAll('.timeline-point'),
+        targetPoint = document.querySelector(`.timeline [data-timeline-num='${slideNum}']`)
+
+    allPoints.forEach(point => {
+        point.classList.remove('active')
+    })
+    targetPoint.classList.add('active')
+
 })
