44a9b8f95506cd0d98d6367cd03093e17a71972a
Viktor Szépe <viktor@szepe.net>
2023-08-14 06:06:37 +0200

    Improve start.sh

--- start.sh
+++ start.sh
@@ -31,10 +31,10 @@ FILE=composer.json
 if [ ! -f "$FILE" ]; then
   echo "No composer.json found."
   exit
-else
-  echo "I found composer.json. Let's begin downloading required libraries."
-  sleep 2
 fi
+
+echo "I found composer.json. Let's begin downloading required libraries."
+sleep 2
 
 echo "# DATABASE" >> .env;
 read -p "Enter db name: " dbname
@@ -48,8 +48,8 @@ echo "DB_HOST=$dbhost" >> .env;
 read -e -p "Enter db prefix (default: wp_): " -i "wp_" dbprefix
 echo "DB_PREFIX=$dbprefix" >> .env;
 echo "#/ DATABASE" >> .env;
-echo "" >> .env;
-echo "" >> .env;
+echo >> .env;
+echo >> .env;
 echo "# ENV" >> .env;
 echo "WP_ENV=development" >> .env
 read -p "Enter local domain name (eg. wordpress.local): " domain
@@ -58,23 +58,17 @@ echo 'WP_SITEURL=${WP_HOME}/wp' >> .env;
 read -p "Enter ACF PRO KEY: " acfprokey
 echo "ACF_PRO_KEY=$acfprokey" >> .env
 echo "#/ ENV" >> .env;
-echo "" >> .env;
-echo "" >> .env;
+echo >> .env;
+echo >> .env;
 
 htaccess="$(cat '.htaccess')"
 > '.htaccess'
 echo "$htaccess" | sed -r "s/juniper.local/$domain/g" >> '.htaccess'
 
 echo "# SALTS" >> .env;
-wget -qO /tmp/wp.keys https://api.wordpress.org/secret-key/1.1/salt/
-echo "AUTH_KEY=\"$(cat /tmp/wp.keys |grep -w AUTH_KEY | cut -d \' -f 4)\"" >>  .env
-echo "SECURE_AUTH_KEY=\"$(cat /tmp/wp.keys |grep -w SECURE_AUTH_KEY | cut -d \' -f 4)\"" >>  .env
-echo "LOGGED_IN_KEY=\"$(cat /tmp/wp.keys |grep -w LOGGED_IN_KEY | cut -d \' -f 4)\"" >>  .env
-echo "NONCE_KEY=\"$(cat /tmp/wp.keys |grep -w NONCE_KEY | cut -d \' -f 4)\"" >>  .env
-echo "AUTH_SALT=\"$(cat /tmp/wp.keys |grep -w AUTH_SALT | cut -d \' -f 4)\"" >>  .env
-echo "SECURE_AUTH_SALT=\"$(cat /tmp/wp.keys |grep -w SECURE_AUTH_SALT | cut -d \' -f 4)\"" >>  .env
-echo "LOGGED_IN_SALT=\"$(cat /tmp/wp.keys |grep -w LOGGED_IN_SALT | cut -d \' -f 4)\"" >>  .env
-echo "NONCE_SALT=\"$(cat /tmp/wp.keys |grep -w NONCE_SALT | cut -d \' -f 4)\"" >>  .env
+wget -qO - https://api.wordpress.org/secret-key/1.1/salt/ \
+  | tr "'" '"' \
+  | sed -n -e 's#^define("\([A-Z_]\+\)",\s\+\("[^"]\+"\));$#\1=\2#p' >> .env
 echo "#/ SALTS" >> .env;
 
 composer install --ignore-platform-reqs
