3685a686b4d059aeac1dae75f28e6c5605d39949
Will Nahmens <will@wp-stars.com>
2023-08-23 09:18:55 +0200

    catchup

--- web/app/themes/juniper-theme/functions.php
+++ web/app/themes/juniper-theme/functions.php
@@ -157,36 +157,48 @@ function wps_add_to_context( $context ) {
 }
 
 
-// Allow SVG
-add_filter( 'wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
+/**
+ * Mime Type SVG enablen zum Upload
+ */
+add_filter('upload_mimes', 'wps_mime_type_svg');
 
-    global $wp_version;
-    if ( $wp_version !== '4.7.1' ) {
-       return $data;
-    }
-  
-    $filetype = wp_check_filetype( $filename, $mimes );
-  
-    return [
-        'ext'             => $filetype['ext'],
-        'type'            => $filetype['type'],
-        'proper_filename' => $data['proper_filename']
-    ];
-  
-  }, 10, 4 );
-  
-  function cc_mime_types( $mimes ){
+function wps_mime_type_svg($mimes) {
+    
     $mimes['svg'] = 'image/svg+xml';
+	$mimes['ico'] = 'image/x-icon';
     return $mimes;
-  }
-  add_filter( 'upload_mimes', 'cc_mime_types' );
+}
+
+// // Allow SVG
+// add_filter( 'wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
+
+//     global $wp_version;
+//     if ( $wp_version !== '4.7.1' ) {
+//        return $data;
+//     }
   
-  function fix_svg() {
-    echo '<style type="text/css">
-          .attachment-266x266, .thumbnail img {
-               width: 100% !important;
-               height: auto !important;
-          }
-          </style>';
-  }
-  add_action( 'admin_head', 'fix_svg' );
+//     $filetype = wp_check_filetype( $filename, $mimes );
+  
+//     return [
+//         'ext'             => $filetype['ext'],
+//         'type'            => $filetype['type'],
+//         'proper_filename' => $data['proper_filename']
+//     ];
+  
+//   }, 10, 4 );
+  
+//   function cc_mime_types( $mimes ){
+//     $mimes['svg'] = 'image/svg+xml';
+//     return $mimes;
+//   }
+//   add_filter( 'upload_mimes', 'cc_mime_types' );
+  
+//   function fix_svg() {
+//     echo '<style type="text/css">
+//           .attachment-266x266, .thumbnail img {
+//                width: 100% !important;
+//                height: auto !important;
+//           }
+//           </style>';
+//   }
+//   add_action( 'admin_head', 'fix_svg' );
