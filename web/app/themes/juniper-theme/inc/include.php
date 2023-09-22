0e4240e61c85b30ec0f42f1a7cc268130979e2c8
Will Nahmens <will@wp-stars.com>
2023-09-22 14:32:49 +0200

    fix references slider

--- web/app/themes/juniper-theme/inc/include.php
+++ web/app/themes/juniper-theme/inc/include.php
@@ -4,3 +4,4 @@ $juniper_blocks = new \Juniper\Blocks\JuniperBlocks();
 $juniper_blocks->include_blocks_functions();
 $juniper_cpt_jobs = new \Juniper\Cpt\Jobs();
 $juniper_cpt_projects = new \Juniper\Cpt\Projects();
+$juniper_cpt_services = new \Juniper\Cpt\Services();
