6f5982ca630e79e7688ef8a2e2fdf3428075172d
Valentin Paul <Valentin@Valentin-Paul.local>
2023-09-28 13:46:12 +0200

    breadcrumb widget for desktop done

--- web/app/themes/juniper-theme/blocks/breadcrumbs/functions.php
+++ web/app/themes/juniper-theme/blocks/breadcrumbs/functions.php
@@ -15,6 +15,33 @@ add_action(
 add_filter(
     'timber/acf-gutenberg-blocks-data/breadcrumbs',
     function ( $context ) {
+        
+         // title and URL of current page
+         $page_title = get_the_title();
+         $page_url = get_permalink();
+
+        // title and URL of parent page
+        $parent_page_title = '';
+        $parent_page_url = '';
+        if (is_page() && $post = get_post()) {
+            $parent_page_id = $post->post_parent;
+            if ($parent_page_id) {
+                $parent_page = get_post($parent_page_id);
+                $parent_page_title = get_the_title($parent_page);
+                $parent_page_url = get_permalink($parent_page);
+            }
+        }
+
+         // URL of home page
+         $home_page_url = home_url();
+
+        // Add pages to context
+     $context['home_page_url'] = $home_page_url;
+     $context['page_title'] =   $page_title;
+     $context['page_url'] =   $page_url;
+     $context['parent_page_title'] = $parent_page_title;
+     $context['parent_page_url'] = $parent_page_url;
+
         return $context;
     }
 );
