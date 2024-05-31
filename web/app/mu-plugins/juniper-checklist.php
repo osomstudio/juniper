e59017f289b48f702df02090336100c306caf67b
Bartosz Nowak <bnowak10@gmail.com>
2024-05-31 10:54:07 +0200

    Juniper Checklist Optimization

---
+++ web/app/mu-plugins/juniper-checklist.php
@@ -0,0 +1,24 @@
+<?php
+
+function juniper_disable_emojis() {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+	add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+	add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
+}
+
+add_action( 'init', 'juniper_disable_emojis' );
+
+add_filter( 'xmlrpc_enabled', '__return_false' );
+
+
+function juniper_remove_wp_version() {
+	return '';
+}
+add_filter( 'the_generator', 'juniper_remove_wp_version' );
+remove_action( 'wp_head', 'wp_generator' );
