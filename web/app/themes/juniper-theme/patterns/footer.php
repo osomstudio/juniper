9c5f25f3236ec01600e55fdf260b5fb074591ac7
Kamil <kamil@osoms-MacBook-Air.local>
2024-09-11 15:33:25 +0200

    Init refactoring juniper to use gutenberg

---
+++ web/app/themes/juniper-theme/patterns/footer.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: Footer
+ * Slug: juniper/footer
+ * Categories: footer
+ * Block Types: core/template-part/footer
+ */
+
+$context = Timber::context();
+Timber::render( 'editor/footer.twig', $context );
